988951b820e5db15bf5548cbb006b2f580f51874
mdkhaja <khajaminuddinmdk@gmail.com>
2022-05-16 12:45:27 +0530

    git commit

---
+++ 16-May/Q1.py
@@ -0,0 +1,3 @@
+lst = [10, 11, 12, 13, 14, 15]
+revlst=lst[::-1]
+print(revlst)
---
+++ 16-May/Q2.py
@@ -0,0 +1,4 @@
+list1 = ["M", "na", "i", "K"]
+list2 = ["y", "me", "s", "haja"]
+list3 = [i + j for i, j in zip(list1, list2)]
+print(list3)
